fcfb760bc7b0296eeeb0dc28aa0bbc181f44107d
Vitor Albarado <vitor.jardina@gmail.com>
2023-06-20 16:08:37 -0400

    add files

---
+++ src/main/java/br/com/restapi/apigetway/Greenting.java
@@ -0,0 +1,37 @@
+package br.com.restapi.apigetway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("greeting")
+public class Greenting {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+
+
+}
